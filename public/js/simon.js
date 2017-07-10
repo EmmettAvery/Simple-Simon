008d205e588868e2e7e76334ed8b77ea25f9589d
Avery Hankins <averyhankins@Averys-MacBook-Pro.local>
2017-07-10 15:19:33 -0500

    Finish functionality

--- public/js/simon.js
+++ public/js/simon.js
@@ -3,7 +3,10 @@ var arr = [];
 var playerVal;
 var x = 0;
 var simon = [];
-var boxArray = [$("#0"),$("#1"),$("#2"),$("#3")];
+var opt1 = $("#0");
+var opt2 = $('#1');
+var opt3 = $('#2');
+var opt4 = $('#3');
 var imgs = ["imgs/ss1.jpeg", "imgs/ss2.jpeg", "imgs/ss3.jpeg", "imgs/ss4.jpeg", "imgs/ss5.jpeg", "imgs/ss6.jpeg", "imgs/ss7.jpeg", "imgs/ss8.jpeg", "imgs/ss9.jpeg", "imgs/ss10.jpeg", "imgs/ss11.jpeg", "imgs/ss12.jpeg"];
 // var num1 = $('#num1').html().val();
 // var num2 = $('#num2').html().val();
@@ -15,31 +18,56 @@ var imgs = ["imgs/ss1.jpeg", "imgs/ss2.jpeg", "imgs/ss3.jpeg", "imgs/ss4.jpeg", 
     }
 function populate(x){
 	for (var i = 0; i < x; i++){
-		var rand = Math.floor(Math.random()* 3);
+		var rand = Math.floor(Math.random()* 4);
 		simon.push(rand);
 	}
 	console.log(simon);
 }
-function randomBox(){
-	var x = 0;
-	for (var i = 0; i < simon.length; i++){
-	function startIMG(i){
-       		boxArray[simon[i]].html("<img src=" + imgRandom(imgs) + ">");
-		console.log("1");
+function display(){
+var i = 0;
+	function plsno(){
+	var godkillme = setInterval(function() {
+	switch (simon[i]){
+		case 0:
+		opt1.html("<img src=" + imgRandom(imgs) + ">");
+		setTimeout(function(){
+			opt1.html("<img src=>");
+		},1000);
+		break;
+		case 1:
+		opt2.html("<img src=" + imgRandom(imgs) + ">");
+		setTimeout(function(){
+			opt2.html("<img src=>");
+		},1000);
+		break;
+		case 2:
+		opt3.html("<img src=" + imgRandom(imgs) + ">");
+		setTimeout(function(){
+			opt3.html("<img src=>");
+		},1000);
+		break;
+		case 3:
+		opt4.html("<img src=" + imgRandom(imgs) + ">");
+		setTimeout(function(){
+			opt4.html("<img src=>");
+		},1000);
+		break;
 	}
-	function stopIMG(i){
-			boxArray[simon[i]].html("<img src=>");
-		console.log("2");
+	i++;
+	if(i >= simon.length){
+		clearInterval(godkillme);
 	}
-	}
-    console.log("3");
-    };
-function resetGame(){
-	arr = [];
-	populate(2);
-	randomBox();
+	}, 1050);
 }
+plsno();
 
+}
+function resetGame(){
+	simon = [];
+	populate(1);
+	display();
+}
+$('input').val(simon.length);
 //checking for proper values.
         //listener for input{
         $( ".boxes" ).each(function(index) {
@@ -53,13 +81,14 @@ function resetGame(){
 					x = 0;
 					alert("You lose!");
 					resetGame();
+					$('input').val(0);
 				}
-				if (x == arr.length-1){
+				if (x == simon.length){
 					alert("Next level!");
 					populate(1);
-					for (var y = 0; y < simon.length; y++){
-					randomBox();
-				}
+					display();
+				x = 0;
+				$('input').val(simon.length -1);
 			}
         	})
     	})
